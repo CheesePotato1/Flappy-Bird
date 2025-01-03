bd64de9d24007fd391c6eee96b42621ea0515cbf
CheesePotato1 <135465162+CheesePotato1@users.noreply.github.com>
2025-01-02 18:20:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title('Flappy Bird')
+st.write('Welcome to the game!')
+
+# Add a simple button to test
+if st.button('Click me'):
+    st.write('You clicked the button!')
